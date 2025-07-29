f0d0695e17977ef2cb434ad51bcdcf5bbe835738
totomakers <totomakers@gmail.com>
2025-07-29 09:32:24 +0200

    :sparkles: Try better auth

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,13 @@
 import fastify from 'fastify'
+import { authPlugin } from './plugins/auth.js'
 
 const server = fastify()
 
 server.get('/', () => {
   return 'Hello world'
 })
+
+server.register(authPlugin)
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
